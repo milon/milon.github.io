94e9986c4a69dac859701b1596c4294261dfbb3d
Nuruzzaman Milon <contact@milon.im>
2018-01-05 09:14:31 +0600

    added image on about me page

--- source/about-me.blade.php
+++ source/about-me.blade.php
@@ -4,7 +4,10 @@
     <h2>About Me</h2>
     <p></p>
 
-    <p>Birth, childhood everything is in Dhaka. Got education from Shamsul Haque Khan High School and Dhaka College. Then went to Mawlana Bhashani Science and Technology University to study Information and Communication Technology.</p>
+    <p>
+        <img src="/images/avatar.jpg" alt="Avatar" class="avatar">
+        Birth, childhood everything is in Dhaka. Got education from Shamsul Haque Khan High School and Dhaka College. Then went to Mawlana Bhashani Science and Technology University to study Information and Communication Technology.
+    </p>
     <p>I have interest in every single thing on the earth except formal education. Loved to be in touch with computer and technology. Had a weakness on computer programming. Recently very excited with cars.</p>
     <p>I love to hang out with my buddies. I regularly go to <a href="https://www.facebook.com/koratkol">Kortaltol University</a>, the best hangout place on earth.</p>
     <p>Currently, I am working for a <a href="https://telenorhealth.com">multinational company</a> as Senior Software Engineer. I am also an admin of <a href="https://www.facebook.com/groups/pxperts">phpXperts</a>, the largest developer community of Bangladesh.</p>
