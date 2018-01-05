c55f79fdade33f48c9666477956750c58b2b9ab4
Nuruzzaman Milon <contact@milon.im>
2018-01-05 08:09:54 +0600

    update laravel page

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,6 +19,7 @@
             <h2>Programmer, Auther, Tech Enthusiast</h2>
             <p>
                 <a href="/about-me">About Me</a>
+                <a href="/laravel">Book</a>
                 <a href="/contact">Contact</a>
                 <a href="/resume">Resume</a>
             </p>
