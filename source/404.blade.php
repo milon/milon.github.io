8a15e2efe89977bde30ad19e0e3d06ef1e753cd1
Nuruzzaman Milon <contact@milon.im>
2018-05-28 20:52:55 +0200

    added custom 404 page

---
+++ source/404.blade.php
@@ -0,0 +1,10 @@
+@extends('_layouts.master')
+
+@section('body')
+    <h2>404</h2>
+
+    <div class="contact">
+        <p>Opps! It seems you are lost.</p>
+        <p>Go to the <a href="/">homepage</a> instead.</p>
+    </div>
+@endsection
