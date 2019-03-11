6531538dd0f41f4145b4dedb1069cdd914374b73
Nuruzzaman Milon <nuruzzaman.milon@check24.de>
2019-03-11 11:55:12 +0100

    added facebook meta tags

---
+++ source/_layouts/_facebook_meta.blade.php
@@ -0,0 +1,5 @@
+<meta property="og:url" content="{{ $page->getUrl() }}" />
+<meta property="og:type" content="Article" />
+<meta property="og:title" content="{{ $page->title }}" />
+<meta property="og:description" content="{{ $page->gist }}" />
+<meta property="og:image" content="{{ $page->baseUrl.'images/qr-code.png' }}" />
