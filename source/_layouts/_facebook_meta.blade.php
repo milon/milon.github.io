d70694ef28ff4b75b948a6d45f0613dccb94b1ae
Nuruzzaman Milon <nuruzzaman.milon@check24.de>
2019-03-11 12:02:59 +0100

    added facebook meta tag app id

--- source/_layouts/_facebook_meta.blade.php
+++ source/_layouts/_facebook_meta.blade.php
@@ -3,3 +3,4 @@
 <meta property="og:title" content="{{ $page->title }}" />
 <meta property="og:description" content="{{ $page->gist }}" />
 <meta property="og:image" content="{{ $page->baseUrl.'images/qr-code.png' }}" />
+<meta property="fb:app_id" content="264496574269710" />
