b7f07dd0c4882f58b60baf348bd886821bf62ad1
Nuruzzaman Milon <contact@milon.im>
2019-09-10 14:56:46 +0200

    added flag emoji

--- source/index.blade.php
+++ source/index.blade.php
@@ -23,5 +23,5 @@
     <p>I love to hang out with my buddies. I regularly go to <a href="https://www.facebook.com/koratkol">Kortaltol University</a>, the best hangout place on earth.</p>
     <p>Currently, I am working for a German <a href="https://urbansportsclub.com">company</a> as Senior Software Engineer and living in Berlin, Germany. I am also an admin of <a href="https://www.facebook.com/groups/pxperts">phpXperts</a>, the largest developer community of Bangladesh.</p>
     <p>Recently I become a father of a beautiful prince.</p>
-    <p>I am a proud <a href="https://en.wikipedia.org/wiki/Bangladesh">Bangladeshi</a>.</p>
+    <p>I am a proud <a href="https://en.wikipedia.org/wiki/Bangladesh">Bangladeshi 🇧🇩</a>.</p>
 @endsection
