592a75b591085ececc2385e4260f5f6a31c71298
Nuruzzaman Milon <contact@milon.im>
2019-09-16 14:07:13 +0200

    added laravel mix

---
+++ webpack.mix.js
@@ -0,0 +1,12 @@
+let mix = require('laravel-mix');
+let build = require('./tasks/build.js');
+
+mix.disableSuccessNotifications();
+mix.setPublicPath('source/assets/build');
+mix.webpackConfig({
+    plugins: [
+        build.jigsaw,
+        build.browserSync(),
+        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
+    ]
+});
