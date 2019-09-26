beac636b8a078db1bdefcaf4e750958d5516e68e
Nuruzzaman Milon <contact@milon.im>
2019-09-26 16:13:40 +0200

    remove unwanted mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,13 +3,6 @@ let build = require('./tasks/build.js');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
-mix.webpackConfig({
-    plugins: [
-        build.jigsaw,
-        build.browserSync(),
-        build.watch(['source/**/*.md', 'source/**/*.php', 'source/**/*.scss', '!source/**/_tmp/*']),
-    ]
-});
 
 mix.sass('source/_assets/sass/main.scss', 'css/main.css')
     .sourceMaps()
