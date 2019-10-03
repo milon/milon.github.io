cff4daf26b7d3f4a82e6f2766c1a9cd1abed6302
Nuruzzaman Milon <contact@milon.im>
2019-10-03 22:31:33 +0200

    fix font icons

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/zsh
 
 npm run prod
+rm -rf assets/build/fonts/vendor
 ./vendor/bin/jigsaw build production
 git add build_production
 git commit -m "Build for deploy"
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,6 +4,8 @@ let build = require('./tasks/build.js');
 mix.disableNotifications();
 mix.setPublicPath('source/assets/build');
 
+mix.copyDirectory( './node_modules/@fortawesome/fontawesome-free/webfonts', 'source/assets/build/fonts/font-awesome' )
+
 mix.sass('source/_assets/sass/main.scss', 'css/main.css')
     .sourceMaps()
     .sass('source/_assets/sass/cv.scss', 'css/cv.css')
