26a206cc10bdf17ff5540aa5920f0aefaa21d56c
Nuruzzaman Milon <contact@milon.im>
2019-10-03 23:22:19 +0200

    clean up fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,12 +4,11 @@ let build = require('./tasks/build.js');
 mix.disableNotifications();
 mix.setPublicPath('source/assets/build');
 
-mix.copyDirectory( './node_modules/@fortawesome/fontawesome-free/webfonts', 'source/assets/build/fonts/font-awesome' )
-
 mix.sass('source/_assets/sass/main.scss', 'css/main.css')
     .sourceMaps()
     .sass('source/_assets/sass/cv.scss', 'css/cv.css')
     .sourceMaps()
     .sass('source/_assets/sass/resume.scss', 'css/resume.css')
     .sourceMaps()
+    .copyDirectory('./node_modules/@fortawesome/fontawesome-free/webfonts', 'source/assets/build/fonts/font-awesome')
     .version();
