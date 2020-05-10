a7fdc01c4b227b3e98f640f73bc04e5aa10bcdf7
Nuruzzaman Milon <contact@milon.im>
2020-05-10 11:56:51 +0200

    fix pagination

--- source/_layouts/_partials/_pagination.blade.php
+++ source/_layouts/_partials/_pagination.blade.php
@@ -7,12 +7,47 @@
         <span><i class="fas fa-angle-left"></i></span>
     @endif
 
-    @foreach ($pagination->pages as $pageNumber => $path)
-        <a href="{{ $path }}"
-        class="{{ $pagination->currentPage == $pageNumber ? 'selected' : '' }}">
-            {{ $pageNumber }}
-        </a>
-    @endforeach
+    @if ($pagination->currentPage <= 3)
+        @foreach ($pagination->pages as $pageNumber => $path)
+            @if($pageNumber <= 5)
+                <a href="{{ $path }}"
+                class="{{ $pagination->currentPage == $pageNumber ? 'selected' : '' }}">
+                    {{ $pageNumber }}
+                </a>
+            @endif
+        @endforeach
+
+        @if($pagination->totalPages > 5)
+            <span>...</span>
+        @endif
+    @elseif ($pagination->currentPage >= ($pagination->totalPages - 2))
+        @if($pagination->totalPages > 5)
+            <span>...</span>
+        @endif
+        @foreach ($pagination->pages as $pageNumber => $path)
+            @if($pageNumber > ($pagination->totalPages - 5))
+                <a href="{{ $path }}"
+                class="{{ $pagination->currentPage == $pageNumber ? 'selected' : '' }}">
+                    {{ $pageNumber }}
+                </a>
+            @endif
+        @endforeach
+    @else
+        @if($pagination->totalPages > 5)
+            <span>...</span>
+        @endif
+        @foreach ($pagination->pages as $pageNumber => $path)
+            @if($pageNumber >= ($pagination->currentPage - 2) && $pageNumber <= ($pagination->currentPage + 2))
+                <a href="{{ $path }}"
+                class="{{ $pagination->currentPage == $pageNumber ? 'selected' : '' }}">
+                    {{ $pageNumber }}
+                </a>
+            @endif
+        @endforeach
+        @if($pagination->totalPages > 5)
+            <span>...</span>
+        @endif
+    @endif
 
     @if ($next = $pagination->next)
         <a href="{{ $next }}"><i class="fas fa-angle-right"></i></a>
