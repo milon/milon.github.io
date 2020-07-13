9208c881888aecb160ffcf834b06cd06bc5fc26e
Nuruzzaman Milon <contact@milon.im>
2020-07-13 22:10:29 +0200

    delete unnecessary files

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/zsh
-
-npm run prod
-./vendor/bin/jigsaw build production
-git add build_production
-git commit -m "Build for deploy"
-git subtree push --prefix build_production origin master
