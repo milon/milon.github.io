9bc2f09cfa134b03afe54a3718259fb81a6627c9
Nuruzzaman Milon <contact@milon.im>
2020-12-30 20:25:28 +0100

    fix typo

--- source/index.blade.php
+++ source/index.blade.php
@@ -17,6 +17,7 @@
     <p>I have interest in every single thing on the earth except formal education. Loved to be in touch with computer and technology. Had a weakness on computer programming. Recently very excited with cars.</p>
     <p>I love to hang out with my buddies. I used to go to <a href="https://www.facebook.com/koratkol">Koratkol University</a> regularly, the best hangout place on earth. Unfortunately, I can't do it any more because I moved out from Bangladesh.</p>
     <p>Currently, I am working for a German <a href="https://global.flixbus.com/">company</a> as Senior Full-Stack Software Engineer and living in Berlin, Germany. I am also an admin of <a href="https://www.facebook.com/groups/pxperts">phpXperts</a>, the largest developer community of Bangladesh.</p>
-    <p>Recently I become a father of a beautiful prince.</p>
+    <p>Recently, I have a new hobby for cooking. I am sharing my recipes <a href="https://easy-recipes.netlify.app/">here</a>.</p>
+    <p>I am the father of a beautiful prince.</p>
     <p>I am a proud <a href="https://en.wikipedia.org/wiki/Bangladesh">Bangladeshi 🇧🇩</a>.</p>
 @endsection
