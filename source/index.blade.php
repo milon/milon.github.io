c4d471fd0d2fbe9fde0c1fb761ad0b3e98afadac
Nuruzzaman Milon <contact@milon.im>
2022-08-26 20:02:17 +0200

    update index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -18,7 +18,7 @@
     <p>I love to hang out with my buddies. I used to go to <a href="https://www.facebook.com/koratkol">Koratkol University</a> regularly, the best hangout place on earth. Unfortunately, I can't do it any more because I moved out from Bangladesh.</p>
     <p>Currently, I am working for a German <a href="https://global.flixbus.com/">company</a> as Senior Full-Stack Software Engineer and living in Berlin, Germany. I am also an admin of <a href="https://www.facebook.com/groups/pxperts">phpXperts</a>, the largest developer community of Bangladesh.</p>
     <p>Recently, I have a new hobby for cooking. I am sharing my recipes <a href="https://easy-recipes.netlify.app/">here</a>.</p>
-    <p>I am the father of a beautiful prince.</p>
+    <p>I am the father of a beautiful prince and a princess.</p>
     <p>I am a proud <a href="https://en.wikipedia.org/wiki/Bangladesh">Bangladeshi 🇧🇩</a>.</p>
     <p class="license">Everything of this site is published under <a rel="license" href="http://creativecommons.org/licenses/by/2.0/"><i class="fab fa-creative-commons"></i> Creative Commons Attribution 2.0 Generic License</a>.</p>
 @endsection
