53d765c911c004c47d0bbc5f87d2974992596385
Nuruzzaman Milon <contact@milon.im>
2023-10-10 15:02:13 -0400

    update recipe site url

--- source/index.blade.php
+++ source/index.blade.php
@@ -17,7 +17,7 @@
     <p>I have a deep curiosity for a wide range of subjects, although formal education hasn't always been my strong suit. My passion lies in computers and technology, with a particular fondness for computer programming. Lately, I've developed a keen interest in cars.</p>
     <p>Spending time with my friends is something I cherish. In the past, I used to frequent <a href="https://www.facebook.com/koratkol">Koratkol University</a>, which I considered the ultimate hangout spot. Unfortunately, I can no longer do so since I relocated from Bangladesh.</p>
     <p>Currently, I'm employed as a Software Development Engineer II at <a href="https://amazon.com">Amazon</a>, residing in Toronto, Ontario, Canada. Additionally, I serve as an administrator for <a href="https://www.facebook.com/groups/pxperts">phpXperts</a>, Bangladesh's largest developer community.</p>
-    <p>Lately, I've developed a newfound passion for cooking, and I'm excited to share my recipes <a href="/recipe">here</a>.</p>
+    <p>Lately, I've developed a newfound passion for cooking, and I'm excited to share my recipes <a href="https://recipes.milon.im">here</a>.</p>
     <p>I am blessed to be the parent of a handsome prince and a lovely princess, whose presence has brought immense joy into my life.</p>
     <p>I wear my <a href="https://en.wikipedia.org/wiki/Bangladesh">Bangladeshi 🇧🇩</a> identity with great pride.</p>
     <p class="license">Everything of this site is published under <a rel="license" href="http://creativecommons.org/licenses/by/2.0/"><i class="fab fa-creative-commons"></i> Creative Commons Attribution 2.0 Generic License</a>.</p>
