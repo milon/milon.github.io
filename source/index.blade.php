eaa3283b2dd1c4c1a22bdf311c26f6ff98250b2a
Nuruzzaman Milon <contact@milon.im>
2025-06-09 10:00:25 -0700

    Update family photo

--- source/index.blade.php
+++ source/index.blade.php
@@ -19,7 +19,7 @@
     <p>Spending time with my friends is something I cherish. In the past, I used to frequent <a href="https://www.facebook.com/koratkol">Koratkol University</a>, which I considered the ultimate hangout spot. Unfortunately, I can no longer do so since I relocated from Bangladesh.</p>
     <p>Currently, I'm employed as a Software Development Engineer II at <a href="https://amazon.com">Amazon</a>, based on Vancouver, British Columbia, Canada and residing in one of the suburbs of Vancouver. Additionally, I serve as an administrator for <a href="https://www.facebook.com/groups/pxperts">phpXperts</a>, Bangladesh's largest developer community.</p>
     <p>Lately, I've developed a newfound passion for cooking, and I'm excited to share my recipes <a href="https://recipes.milon.im">here</a>.</p>
-    <p>I am blessed to be the parent of a handsome prince and a lovely princess, whose presence has brought immense joy into my life.</p>
+    <p>I am blessed to be the parent of a handsome prince and two lovely princesses, whose presence has brought immense joy into my life.</p>
     <p>I wear my <a href="https://en.wikipedia.org/wiki/Bangladesh">Bangladeshi 🇧🇩</a> identity with great pride.</p>
     <p class="license">Everything of this site is published under <a rel="license" href="http://creativecommons.org/licenses/by/2.0/"><i class="fab fa-creative-commons"></i> Creative Commons Attribution 2.0 Generic License</a>.</p>
     </div>
