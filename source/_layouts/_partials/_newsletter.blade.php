57d42ca00d7a7e1ac0acf150bf1c4bb1c68d4ce3
milon <milon@punt.com>
2026-08-21 00:21:50 -0700

    Update newsletter form

--- source/_layouts/_partials/_newsletter.blade.php
+++ source/_layouts/_partials/_newsletter.blade.php
@@ -1,7 +1,7 @@
 <div class="newsletter">
     <h2>Occasional notes, no spam</h2>
     <p class="newsletter-intro">Want to know about my thoughts and what's going on? Subscribe below. One email when there's something worth sending, and nothing otherwise.</p>
-    <form action="https://nuruzzaman-milon.kit.com/347d5a3bdb/subscriptions" method="post">
+    <form action="https://app.kit.com/forms/9731057/subscriptions" method="post">
         <p>
             <input class="email" type="email" name="email_address" placeholder="you@example.com" required />
             <button type="submit" class="submit-btn">Subscribe</button>
