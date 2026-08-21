de2fb07c4889e76f3c70f057c6d647bd4a69d710
milon <milon@punt.com>
2026-08-21 13:27:40 -0700

    Enable highlight.js

--- source/_layouts/_partials/_highlightjs.blade.php
+++ source/_layouts/_partials/_highlightjs.blade.php
@@ -1,5 +1,8 @@
-<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.11.2/highlight.min.js"></script>
 
 <script>
-    hljs.initHighlightingOnLoad();
+    // Only blocks that declared a language. Auto-detection guesses badly on things
+    // like psql output, and colours it as whatever language it looks closest to.
+    hljs.configure({ cssSelector: 'pre code[class*="language-"]' });
+    hljs.highlightAll();
 </script>
